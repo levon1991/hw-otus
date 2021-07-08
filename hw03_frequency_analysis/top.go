e78bd76e9d630d008dab02e456b06d4f75f3ebb6
Levon Baghramyan <levon.baghramyan@yandex.ru>
2021-07-08 16:10:09 +0400

    HW3 is completed

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -1,6 +1,53 @@
 package hw03frequencyanalysis
 
-func Top10(_ string) []string {
-	// Place your code here.
-	return nil
+import (
+	"regexp"
+	"sort"
+	"strings"
+)
+
+type Word struct {
+	Name  string
+	count int
+}
+
+func Top10(text string) []string {
+	sliceList := make([]string, 0)
+
+	// delete all symbols like '\n', '\t', double space
+	space := regexp.MustCompile(`\s+`)
+	res := space.ReplaceAllString(text, " ")
+
+	// check for empty string
+	if len(res) == 0 || res == " " {
+		return sliceList
+	}
+
+	// use map just for calculate words count
+	split := strings.Split(res, " ")
+	listMap := make(map[string]int)
+	for _, v := range split {
+		listMap[v]++
+	}
+
+	// put map in struct for sorting
+	w := make([]Word, 0)
+	for key, v := range listMap {
+		w = append(w, Word{Name: key, count: v})
+	}
+
+	// sort by Name and count
+	sort.Slice(w, func(i, j int) bool {
+		if w[i].count == w[j].count {
+			return w[i].Name < w[j].Name
+		}
+		return w[i].count > w[j].count
+	})
+
+	// get first 10 words
+	for i := 0; i < 10 && i < len(w); i++ {
+		sliceList = append(sliceList, w[i].Name)
+	}
+
+	return sliceList
 }
