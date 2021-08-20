ad0d0c5f6612fa5eec5768a1db44435be95bba7b
Levon Baghramyan <levon.baghramyan@yandex.ru>
2021-08-20 16:36:18 +0400

    HW6 is completed

--- hw06_pipeline_execution/pipeline_test.go
+++ hw06_pipeline_execution/pipeline_test.go
@@ -49,6 +49,31 @@ func TestPipeline(t *testing.T) {
 
 		result := make([]string, 0, 10)
 		start := time.Now()
+		for s := range ExecutePipeline(in, nil, stages[1], stages[3]) {
+			result = append(result, s.(string))
+		}
+		elapsed := time.Since(start)
+
+		require.Equal(t, []string{"2", "4", "6", "8", "10"}, result)
+		require.Less(t,
+			int64(elapsed),
+			// ~0.8s for processing 5 values in 4 stages (100ms every) concurrently
+			int64(sleepPerStage)*int64(len(stages)+len(data)-1)+int64(fault))
+	})
+
+	t.Run("use not all stages", func(t *testing.T) {
+		in := make(Bi)
+		data := []int{1, 2, 3, 4, 5, 6}
+
+		go func() {
+			for _, v := range data {
+				in <- v
+			}
+			close(in)
+		}()
+
+		result := make([]string, 0, 10)
+		start := time.Now()
 		for s := range ExecutePipeline(in, nil, stages...) {
 			result = append(result, s.(string))
 		}
