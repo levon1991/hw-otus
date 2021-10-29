cf5802fc7696d29c29fbfe4f8b5b147897d00575
Levon Baghramyan <l.baghramyan@emcd.io>
2021-10-29 12:21:13 +0400

    test commoit

--- hw01_hello_otus/main.go
+++ hw01_hello_otus/main.go
@@ -7,6 +7,20 @@ import (
 
 var msg = "Hello, OTUS!"
 
+func test() bool {
+	return true
+}
 func main() {
 	fmt.Println(stringutil.Reverse(msg))
+	if test() {
+		fmt.Println("1")
+	} else {
+		if !test() {
+			fmt.Println("2")
+		} else if test() == true {
+			fmt.Println("3")
+		} else {
+			fmt.Println("4")
+		}
+	}
 }
